6b98b406fb38d77a18b3556add911b1f35181752
nieyue <278076304@qq.com>
2020-06-03 00:08:57 +0800

    2020 6 3

--- resources/js/base.js
+++ resources/js/base.js
@@ -2,8 +2,12 @@ var business={
 	isPC:function(){
 		if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
 		   //alert('手机端')
+		   $("#nav-wrap").css("width","50%")
+		   $("#nav-wrap-bg").css("width","50%")
 		   return false;
 		}else{
+			$("#nav-wrap").css("width","30%")
+		   $("#nav-wrap-bg").css("width","30%")
 		   //alert('PC端')
 		   return true;
 		}
@@ -11,3 +15,12 @@ var business={
 
 
 }
+
+$("#nav-close").click(function(){
+	$("#nav-wrap").css("display","none")
+	$("#nav-wrap-bg").css("display","none")
+})
+$("#nav-open").click(function(){
+	$("#nav-wrap").css("display","block")
+	$("#nav-wrap-bg").css("display","block")
+})
