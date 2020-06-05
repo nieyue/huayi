319a5730308de7fb698788d17898af80a54546c9
nieyue <278076304@qq.com>
2020-06-05 23:40:13 +0800

    2020 6 5

--- resources/js/resize.js
+++ resources/js/resize.js
@@ -1,8 +1,9 @@
 (function(){ 
     function resizeBaseFontSize(){ 
+      var scale=1639/921;//
       var rootHtml = document.documentElement,
           deviceWidth = rootHtml.clientWidth; 
-      if(deviceWidth > 640){ 
+      if(deviceWidth > 990){ 
          deviceWidth = 640;  
       }  
      // rootHtml.style.fontSize = deviceWidth / 7.5 + "px";  
